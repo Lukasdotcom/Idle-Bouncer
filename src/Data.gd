5b5f35d55b7e4b54ee20f83eec830a7ed9dc5fbd
Lukasdotcom <lukas@lschaefer.xyz>
2022-02-03 12:07:58 -0500

    Adds an autosave

--- src/Data.gd
+++ src/Data.gd
@@ -1,14 +1,31 @@
 extends Node2D
-signal update_game_interface
+signal update_game_interface # Signal for when to update the UI
 
+var first
+const save_file = "user://save.json"
 var money = 0 setget change_money
 var coins = 0 setget change_coins
 var earnings = [0, 10, 50, 400]
 var cost = [0, 100, 500, 4000]
+var boxes = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	var file = File.new()
+	if file.file_exists(save_file):
+		file.open(save_file, File.READ)
+		var _data = parse_json(file.get_as_text())
+		if _data["version"] == "v0.1.3":
+			file.close()
+			money = _data["money"]
+			cost = _data["cost"]
+			for x in _data["boxes"]:
+				var _instance = load("res://src/box.tscn")
+				_instance = _instance.instance()
+				_instance.level = x[1]
+				_instance.startAnimation = x[0]
+				_instance.position = Vector2(512, 300)
+				get_node("/root/Main/Game Field").call_deferred("add_child", _instance)
 
 func change_money(value: int) -> void: # Changes the score
 	money = value
@@ -18,7 +35,20 @@ func change_coins(value: int) -> void: # Changes the score
 	coins = value
 	emit_signal("update_game_interface")
 
-func reset_coins() -> void:
+func reset_coins() -> void: # Resets the coins when space bar is pressed
 	money += coins
 	coins = 0
 	emit_signal("update_game_interface")
+	save()
+
+func save() -> void: # Used to save the game
+	var file = File.new()
+	file.open(save_file, File.WRITE)
+	var _save_data = to_json({
+		"money" : money,
+		"boxes" : boxes,
+		"cost" : cost,
+		"version" : "v0.1.3"
+	})
+	file.store_string(_save_data)
+	file.close()
--- src/box.gd
+++ src/box.gd
@@ -1,13 +1,18 @@
 extends Node2D
-
+onready var animation = $AnimationPlayer
 
 var level: int = 1
+var startAnimation: float = 0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	animation.seek(startAnimation)
+	if not Data.first: # Makes sure to set the start time when the first one is created otherwise will add itself to the saving list
+		Data.first = animation
+	else:
+		Data.boxes.append([animation.current_animation_position - Data.first.current_animation_position, level])
+	Data.save() # Makes sure to save the game right away.
 	var _sprite: Sprite = $Node2D/Sprite
 	_sprite.set_modulate(Color.from_hsv((100+30*level)/360.0, 0.9, 1, 1))
-
-
 
 func _on_hit(area: Area2D) -> void:
 	Data.coins += Data.earnings[level]
