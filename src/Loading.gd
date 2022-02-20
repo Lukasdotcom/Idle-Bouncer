f39531c568a93eef8f5473490ac4d42bd384898c
Lukasdotcom <lukas@lschaefer.xyz>
2022-02-19 22:02:28 -0500

    Fixes bug with loading screen

--- src/Loading.gd
+++ src/Loading.gd
@@ -1,17 +1,14 @@
 extends Control
 
-
-
 # Checks if a save exists and if it does allows a way to reset otherwise will load the game right away.
 func _ready() -> void:
 	var file = File.new()
 	if file.file_exists(Data.save_file):
 		file.open(Data.save_file, File.READ)
 		var _data = parse_json(file.get_as_text())
-		if _data["version"] in ["v0.5.0", "v0.5.1"]:
+		if _data["version"] in ["v0.5.0", "v0.5.1", "v0.5.2"]:
 			$Save.visible = true
 			$Timer.start()
-			
 		else:
 			get_tree().change_scene("res://src/Main.tscn")
 	else:
