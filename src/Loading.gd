d8528ee9b6108b3167ce2aaba6def402146cfa76
Lukasdotcom <lukas@lschaefer.xyz>
2022-02-22 07:55:49 -0500

    Adds a check when loading if the save is a valid json

--- src/Loading.gd
+++ src/Loading.gd
@@ -5,12 +5,15 @@ func _ready() -> void:
 	var file = File.new()
 	if file.file_exists(Data.save_file):
 		file.open(Data.save_file, File.READ)
-		var _data = parse_json(file.get_as_text())
-		if _data["version"] in ["v0.5.0", "v0.5.1", "v0.5.2", "v0.6.0"]:
-			$Save.visible = true
-			$Timer.start()
+		if validate_json(file.get_as_text()):
+			var _data = parse_json(file.get_as_text())
+			if _data["version"] in ["v0.5.0", "v0.5.1", "v0.5.2", "v0.6.0"]:
+				$Save.visible = true
+				$Timer.start()
+			else:
+				Data.reset()
 		else:
-			get_tree().change_scene("res://src/Main.tscn")
+			Data.reset()
 	else:
 		get_tree().change_scene("res://src/Main.tscn")
 
