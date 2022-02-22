cd32b3cf8c48ff3b6c40124c94af453aba3e93ae
Lukasdotcom <lukas@lschaefer.xyz>
2022-02-22 08:08:14 -0500

    Fixes bug with loading screen

--- src/Loading.gd
+++ src/Loading.gd
@@ -5,7 +5,7 @@ func _ready() -> void:
 	var file = File.new()
 	if file.file_exists(Data.save_file):
 		file.open(Data.save_file, File.READ)
-		if validate_json(file.get_as_text()):
+		if validate_json(file.get_as_text()) == "":
 			var _data = parse_json(file.get_as_text())
 			if _data["version"] in ["v0.5.0", "v0.5.1", "v0.5.2", "v0.6.0"]:
 				$Save.visible = true
